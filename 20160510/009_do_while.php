92722719b708d458e13fd7d38e8c022f212b12bf
KeisukeSuzuki <sangidai14_keisuke@yahoo.co.jp>
2016-05-10 12:25:45 +0900

    20160510午前中の研修

---
+++ 20160510/009_do_while.php
@@ -0,0 +1,8 @@
+<?php
+$i=0;
+do {
+  echo "<br>". $i ."<br>";
+  $i++;
+  echo "1足して". "<br>". $i. "<br>";
+} while ( $i<10 )
+?>
---
+++ 20160510/010_swich.php
@@ -0,0 +1,14 @@
+<?php
+$x = 2;
+switch($x){
+  case 1:
+    echo "値は1だね。<br>";
+    break;
+  case 2:
+  case 3:
+    echo "値は2か3だね。<br>";
+    break;
+  default:
+    echo "1でも2でも3でもないね。<br>";
+}
+?>
